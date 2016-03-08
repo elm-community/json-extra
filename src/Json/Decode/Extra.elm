e6338da3cc30fdde2d63bb333012e4c9f2e52518
Richard Feldman <richard.t.feldman@gmail.com>
2016-03-08 10:48:31 -0800

    Improve documentation for |:

--- src/Json/Decode/Extra.elm
+++ src/Json/Decode/Extra.elm
@@ -95,18 +95,105 @@ apply f aDecoder =
             |: ("id" := int)
             |: ("name" := string)
             |: ("address" := string)
-            |: ("city" := string)
-            |: ("state" := string)
 
 
     type alias Location =
         { id : Int
         , name : String
         , address : String
-        , city : String
-        , state : String
         }
 
+If you're curious, here's how this works behind the scenes.
+
+`Location` is a type alias, and type aliases give you a convenience function
+that returns an instance of the record in question. Try this out in `elm repl`:
+
+    > type alias Location = { id : Int, name: String, address: String }
+
+    > Location
+    <function> : Int -> String -> String -> Repl.Location
+
+    > Location 1 "The White House" "1600 Pennsylvania Ave"
+    { id = 1, name = "The White House", address = "1600 Pennsylvania Ave" }
+
+In other words, if you call the `Location` function, passing three arguments,
+it will return a new `Location` record by filling in each of its fields. (The
+argument order is based on the order in which we listed the fields in the
+type alias; the first argument sets `id`, the second argument sets `name`, etc.)
+
+Now try running this through `elm repl`:
+
+    > import Json.Decode exposing (succeed, int, string, (:=))
+
+    > succeed Location
+    <function>
+        : Json.Decode.Decoder
+            (Int -> String -> String -> Repl.Location)
+
+So `succeed Location` gives us a `Decoder (Int -> String -> String -> Location)`.
+That's not what we want! What we want is a `Decoder Location`. All we have so
+far is a `Decoder` that wraps not a `Location`, but rather a function that
+returns a `Location`.
+
+What `|: ("id" := int)` does is to take that wrapped function and pass an
+argument to it.
+
+    > import Json.Decode exposing (succeed, int, string, (:=))
+
+    > ("id" := int)
+    <function> : Json.Decode.Decoder Int
+
+    > succeed Location |: ("id" := int)
+    <function>
+        : Json.Decode.Decoder
+            (String -> String -> Repl.Location)
+
+Notice how the wrapped function no longer takes an `Int` as its first argument.
+That's because `|:` went ahead and supplied one: the `Int` wrapped by the decoder
+`("id" := int)` (which returns a `Decoder Int`).
+
+Compare:
+
+    -- succeed Location
+    Decoder (Int -> String -> String -> Location)
+
+    -- succeed Location |: ("id" := int)
+    Decoder (String -> String -> Location)
+
+We still want a `Decoder Location` and we still don't have it yet. Our decoder
+still wraps a function instead of a plain `Location`. However, that function is
+now smaller by one argument!
+
+Let's repeat this pattern to provide the first `String` argument next.
+
+    -- succeed Location
+    Decoder (Int -> String -> String -> Location)
+
+    -- succeed Location |: ("id" := int)
+    Decoder (String -> String -> Location)
+
+    -- succeed Location |: ("id" := int) |: ("name" := string)
+    Decoder (String -> Location)
+
+Smaller and smaller! Now we're down from `(Int -> String -> String -> Location)`
+to `(String -> Location)`. What happens if we repeat the pattern one more time?
+
+    -- succeed Location
+    Decoder (Int -> String -> String -> Location)
+
+    -- succeed Location |: ("id" := int)
+    Decoder (String -> String -> Location)
+
+    -- succeed Location |: ("id" := int) |: ("name" := string)
+    Decoder (String -> Location)
+
+    -- succeed Location |: ("id" := int) |: ("name" := string) |: ("address" := string)
+    Decoder Location
+
+Having now supplied all three arguments to the wrapped function, it has ceased
+to be a function. It's now just a plain old `Location`, like we wanted all along.
+
+We win!
 -}
 (|:) : Decoder (a -> b) -> Decoder a -> Decoder b
 (|:) =
